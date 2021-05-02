5a3183c504deb40d4428bfe5d84334cbb7d24406
andyanday33 <andyanday33@gmail.com>
2021-05-02 20:38:33 +0300

    update to queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,7 +4,7 @@ const Book = require('../models/book');
 const Author = require('../models/author');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema,
-GraphQLID, GraphQLInt, GraphQLList } = graphql;
+GraphQLID, GraphQLInt, GraphQLList, GraphQLNonNull } = graphql;
 
 
 const BookType = new GraphQLObjectType({
@@ -74,8 +74,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name : {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name : {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve (parent, args){
                 let author = new Author({
@@ -88,9 +88,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: { 
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorid: {type: GraphQLID}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorid: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve (parent, args){
                 let book = new Book({
